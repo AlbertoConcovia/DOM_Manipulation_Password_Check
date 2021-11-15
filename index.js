356cdbedf116e142de986652dca14d26f5c57ee3
albertoconcovia <concovia@gmail.com>
2021-11-15 22:20:09 +0000

    Refactoring added

--- index.js
+++ index.js
@@ -13,20 +13,25 @@ const updateProgressBarValue = (newValue) => {
 const updateProgressBarColour = (inputPasswordLength) => {
   const minValue = 8;
   const maxValue = 12;
-  const newFeedback = document.querySelector("p");
+  let newFeedback = document.querySelector("p");
+  document.querySelector("#progressBar").className = "";
+  document.querySelector("#id_p").className = "class_P";
 
   switch (true) {
     case inputPasswordLength < minValue:
       document.querySelector("#progressBar").classList.add("classProgressBarColorRed");
       newFeedback.textContent = `Should be longer`;
+      document.querySelector("#id_p").classList.add("class_P_Red");
       break;
-    case inputPasswordLength > minValue && inputPasswordLength < maxValue:
+    case inputPasswordLength >= minValue && inputPasswordLength <= maxValue:
       document.querySelector("#progressBar").classList.add("classProgressBarColorOrange");
       newFeedback.textContent = `Pretty good`;
+      document.querySelector("#id_p").classList.add("class_P_Orange");
       break;
     case inputPasswordLength > maxValue:
       console.log(`GREEN - Grrrreat!`);
       document.querySelector("#progressBar").classList.add("classProgressBarColorGreen");
+      document.querySelector("#id_p").classList.add("class_P_Green");
       newFeedback.textContent = `Grrrreat!`;
       break;
   }
