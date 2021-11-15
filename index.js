09e4bd9bdd8a03c29b4750f952a3b02dd97f92c6
albertoconcovia <concovia@gmail.com>
2021-11-15 21:46:45 +0000

    refactoring - color and feedback function added

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-// Given the page is rendered with the correct input field When the user changes
-// A single character in the input field Then the progress bar should update its value
-
-// Given the page is rendered with a progress bar that has a value range of 0-100
-// When the user inputs a password
-// Then the progress bar value should be the number of characters multiplied by 5
-
 const getProgressBarValue = (e) => {
   if (e.target.value === undefined) return console.log(`undefined`);
   let inputPasswordLength = e.target.value.length;
   newValue = inputPasswordLength * 5;
   updateProgressBarValue(newValue);
+  updateProgressBarColour(inputPasswordLength);
 };
 
 const updateProgressBarValue = (newValue) => {
   document.querySelector("#progressBar").value = newValue;
 };
 
-let fpassword = document.getElementById("f-password");
-fpassword.addEventListener("input", getProgressBarValue);
+const updateProgressBarColour = (inputPasswordLength) => {
+  const minValue = 8;
+  const maxValue = 12;
+  const newFeedback = document.querySelector("p");
+
+  switch (true) {
+    case inputPasswordLength < minValue:
+      document.querySelector("#progressBar").classList.add("classProgressBarColorRed");
+      newFeedback.textContent = `Should be longer`;
+      break;
+    case inputPasswordLength > minValue && inputPasswordLength < maxValue:
+      document.querySelector("#progressBar").classList.add("classProgressBarColorOrange");
+      newFeedback.textContent = `Pretty good`;
+      break;
+    case inputPasswordLength > maxValue:
+      console.log(`GREEN - Grrrreat!`);
+      document.querySelector("#progressBar").classList.add("classProgressBarColorGreen");
+      newFeedback.textContent = `Grrrreat!`;
+      break;
+  }
+};
+
+let password = document.getElementById("password");
+password.addEventListener("input", getProgressBarValue);
